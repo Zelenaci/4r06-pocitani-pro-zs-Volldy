d4d944f7d87c3b7fcba13caec9a2e7323b26d340
Voldy <voldanvoldy@gmail.com>
2019-03-25 22:43:29 +0100

    P************************* me fakt už j******* na hlavičku

--- volldy_vypočet.py
+++ volldy_vypočet.py
@@ -1,134 +1,112 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 22 10:29:56 2019
-
-@author: vol35098
-"""
-
 import tkinter as tk
-from tkinter import LabelFrame, Radiobutton, Entry, Label, Button
+from tkinter import Label, LabelFrame, Radiobutton, Entry
 from random import randint
-dobre = 0
-celkem = 0
-vysledek = 0
 
-class Application(tk.Tk):
-    name = 'Výpočet'
+start = tk.Tk()
+start.title("Počítání pro Zš")
+
+dobre=0
+celkem=0
+vysledek=0
+znamenko=tk.StringVar()
+
+#zaklad
+znamenka = LabelFrame(start, text="Vyber si znaménko", padx=5, pady=5)
+znamenka.grid(row=0)
+
+priklad = LabelFrame(start, text="Příklad", padx=5, pady=5)
+priklad.grid(row=1)
+
+statistika = LabelFrame(start, text="Statistika", padx=5, pady=5)
+statistika.grid(row=2)
+#menu pro znamenka
+Radiobutton(znamenka, value="+", variable=znamenko, command=vyberznamenka).pack()
+Radiobutton(znamenka, value="-", variable=znamenko, command=vyberznamenka).pack()
+Radiobutton(znamenka, value="*", variable=znamenko, command=vyberznamenka).pack()
+Radiobutton(znamenka, value="/", variable=znamenko, command=vyberznamenka).pack()
+
+
+#přiklad
+cisloAentry=Entry(priklad, width=4, state="readonly")
+cisloAentry.grid(row=1, column=0)
+
+matematickaoperaceLabel=Label(priklad, text="?")
+matematickaoperaceLabel.grid(row=1, column=1)
+
+cisloBentry=Entry(priklad, width=4, state="readonly")
+cisloBentry.grid(row=1, column=2)
+
+rovnitkoLabel=Label(priklad, text="=")
+rovnitkoLabel.grid(row=1, column=3)
+
+vysledekEntry=Entry(priklad, width=4)
+vysledekEntry.grid(row=1, column=4)
+
+#hodnocení
+novyprikladButt=Button(statistika, text="Nový příklad", command=priklady)
+novyprikladButt.grid(row=2)
+
+statButt=Label(statistika, text="Zkontroluj", command=kontrola)
+statButt.grid(row=3)
+
+statLabel=Label(statistika, text="?")
+statLabel.grid(row=3)
+
+def vyberznamenka():
+    selection=str(znamenko.get())
+    matematickaoperaceLabel.config(text=selection)
+    newpriklad.invoke()
+
+def priklady():
+    if matematickaoperaceLabel ['text'] == "+":
+        cisloAentry = random.randint(1,10)
+        cisloBentry = random.randint(1,10)
+    if matematickaoperaceLabel ['text'] == "-":
+        cisloAentry =  random.randint(0, 99)
+        cisloBentry = random.randint(0, cisloA)
+    if matematickaoperaceLabel ['text'] == "*":
+        cisloAentry =  random.randint(1, 9)
+        cisloBentry = random.randint(1, 9)
+    if matematickaoperaceLabel ['text'] == "/":
+        cisloAentry =  random.randint(1, 9)
+        cisloBentry = random.randint(1, 9)
+    cisloAentry.delete(0, END)
+    cisloAentry.insert(0, str(cisloA))
+    cisloBentry.delete(0,END)
+    cisloBentry.insert(0, str(cisloB))
+    vysledekEntry.delete(0,EDN)
     
-    
-    def __init__(self):
-        super().__init__(className=self.name)
-        self.title(self.name)
-        
-        
-        ###------FUNKCE------###
-        self.funkceFrame = LabelFrame(self, text = "FUNKCE", padx=5, pady=5)
-        self.funkceFrame.pack(anchor=tk.W)
-        self.znamenkoVar= tk.StringVar()
-        self.AVar = tk.StringVar()
-        self.BVar = tk.StringVar()
-        
-        ###
-        Radiobutton(self.funkceFrame, text = '+', variable=self.znamenkoVar, value='+', command=self.plus).pack()
-        Radiobutton(self.funkceFrame, text = '-', variable=self.znamenkoVar, value='-', command=self.minus).pack()
-        Radiobutton(self.funkceFrame, text = '*', variable=self.znamenkoVar, value='*', command=self.krat).pack()
-        Radiobutton(self.funkceFrame, text = '/', variable=self.znamenkoVar, value='/', command=self.deleno).pack()
-        self.znamenkoVar.set('+')
+def kontrola():
+    global dobre
+    global celkem
+    a = int(cisloAentry.get())
+    b = int(cisloBentry.get())
+    vys = vysledekEntry.get()
+    if matematickaoperaceLabel['text'] == "+":
+        vysledek = a+b
+    if matematickaoperaceLabel['text'] == "-":
+        vysledek = a-b
+    if matematickaoperaceLabel['text'] == "*":
+        vysledek = a*b
+    if matematickaoperaceLabel['text'] == "/":
+        vysledek = a/b
+    try:
+        if vysledek == int(vys):
+            dobre+=1
+            celkem+=1
+            newpriklad.invoke()
+            vysledekEntry.delete(0)
+            hlavni.title("Počítání pro základní školy")
+        if vysledek != int(vys):
+            celkem+=1
+            newpriklad.invoke()
+            vysledekEntry.delete(0)
+            hlavni.title("Počítání pro základní školy")
+    except:
+        hlavni.title("Ty jsi chobot")
+        celkem+=1
+        newpriklad.invoke()
+    statLabel.config(text='{0}/{1}'.format(dobre,celkem))
 
 
-        #Button(self.funkceFrame, text="Vypočítej", command=self.zkontroluj, width=10).pack()
-        #Button(self.funkceFrame,text="zkontroluj", command=self.zkontroluj, width=10).pack()
-        
-        ###------FUNKCE------###
-        
-        ###------CISLA------###
-        self.cisloFrame = LabelFrame(self,padx=5,pady=5)
-        self.cisloFrame.pack(anchor=tk.W)
-        ###
-        self.cisloA = Entry(self.cisloFrame, state='readonly', width='3', 
-                            textvariable=self.AVar)
-        self.cisloA.grid(row=4, column=3)
-        
-        self.znamenko = Label(self.cisloFrame, textvariable=self.znamenkoVar, 
-                              width='3')
-        self.znamenko.grid(row=4, column=4)
-        
-        self.cisloB = Entry(self.cisloFrame, state='readonly', width='3', 
-                            textvariable=self.BVar)
-        self.cisloB.grid(row=4, column=5)
-        
-        self.rovnitko = Label(self.cisloFrame, text='=', width='3')
-        self.rovnitko.grid(row=4, column=6)
-        
-        self.vysledek = Entry(self.cisloFrame, width='3')
-        self.vysledek.grid(row=4, column=7)
-
-        self.lblcor= Label(self.cisloFrame, text="")
-        self.lblcor.grid(row=4, column=8)
-
-        self.statistika = Label(self.cisloFrame, text="0/0")
-        self.statistika.grid(row=5, column=4)
-        
-        ###------CISLA------###
-        
-        ###------HODNOCENI------###
-        self.hodnoceFrame = LabelFrame(self, text='HODNOCENÍ', padx=5, pady=5)
-        self.funkceFrame.pack(anchor=tk.W)
-        ###------HODNOCENI------###
-        
-        ###------PROMENA pro znaménko------###
-        self.znamenkoVar.trace('w', self.plus,)
-        
-        
-   # def zmena(self, x, y, z):
-            #pro výpočty
-           #print(self.znamenkoVar.get())
-    #        pass
-         ###------PROMENA pro znaménko------###
-    
-    
-    def plus(self):
-        self.cisloA= randint(0, 99)
-        self.cisloB= randint(0, 99)
-        self.vysledek= self.cisloA+self.cisloB
-        print(self.cisloA, "+", self.cisloB, '=', self.vysledek)
-        return()
-        
-
-    def minus(self):
-        self.cisloA= randint(0, 99)
-        self.cisloB= randint(0, self.cisloA)
-        self.vysledek= self.cisloA-self.cisloB
-        print(self.cisloA, "-", self.cisloB, '=', self.vysledek)
-        return()
-        
-        
-        
-    def deleno(self):
-        self.vysledek= randint(0, 9)
-        self.cisloB= randint(1, 9)
-        self.cisloA= self.cisloB*self.vysledek
-        print(self.cisloA, ":", self.cisloB, '=', self.vysledek)
-        return()
-        
-    def krat(self):
-        self.cisloA= randint(0, 9)
-        self.cisloB= randint(0, 9)
-        self.vysledek= self.cisloA*self.cisloB
-        print(self.cisloA,"x" , self.cisloB, '=', self.vysledek)
-        return()
- 
- 
-
-
-
-
-
-
-
-
-
-
-app = Application()
-app.mainloop()
+start.mainloop()
